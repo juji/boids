2921f9ddd2fe5ef7f62e22c821cae347e43fc724
juji <him@jujiyangasli.com>
2024-11-30 18:34:28 +0700

    calcPerThread: 250

--- src/renderer/index.ts
+++ src/renderer/index.ts
@@ -29,7 +29,7 @@ export class Renderer {
   }
   
   calculators: Worker[] = []
-  calcPerThread = 500 //
+  calcPerThread = 250 //
   calculatorNum = 1
   boidNum = 500
   canvas: HTMLCanvasElement
