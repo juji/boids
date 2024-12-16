3afaa6769b5b19e3369c4a8009e067e69ef3160e
juji <him@jujiyangasli.com>
2024-12-17 03:40:25 +0700

    rand for glsl ins not needed

--- src/renderer/webgl/velocityComp.glsl
+++ src/renderer/webgl/velocityComp.glsl
@@ -32,14 +32,14 @@ uniform float fBoidBoxTop;
 uniform float fBoidBoxFront;
 uniform float fBoidBoxBack;
 
-highp float rand(vec2 co){
-  highp float a = 12.9898;
-  highp float b = 78.233;
-  highp float c = 43758.5453;
-  highp float dt= dot(co.xy ,vec2(a,b));
-  highp float sn= mod(dt,3.14);
-  return fract(sin(sn) * c);
-}
+// highp float rand(vec2 co){
+//   highp float a = 12.9898;
+//   highp float b = 78.233;
+//   highp float c = 43758.5453;
+//   highp float dt= dot(co.xy ,vec2(a,b));
+//   highp float sn= mod(dt,3.14);
+//   return fract(sin(sn) * c);
+// }
 
 
 void main(){
